b69cca71097ccfd8667f340ce4207026a6cbe475
Ricardo Willems <RicardoGWill@gmail.com>
2018-08-15 01:29:36 -0400

    Second commit.  I added a Radio Group and Radio Buttons, along with a Submit Button for them.  I also renamed the app "Checkboxes, Radio Buttons, Toasts" to better match the content of the app.

--- app/src/main/java/com/ricardogwill/checkboxesandtoasts/MainActivity.java
+++ app/src/main/java/com/ricardogwill/checkboxesandtoasts/MainActivity.java
@@ -2,15 +2,24 @@ package com.ricardogwill.checkboxesandtoasts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private CheckBox checkBox1, checkBox2, checkBox3;
 
+    private RadioGroup radioGroup;
+    private RadioButton radioButton1, radioButton2, radioButton3;
+    private Button submitButton;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -19,7 +28,14 @@ public class MainActivity extends AppCompatActivity {
         checkBox2 = (CheckBox) findViewById(R.id.check_box_2);
         checkBox3 = (CheckBox) findViewById(R.id.check_box_3);
 
+        radioGroup = (RadioGroup) findViewById(R.id.radio_group);
+        submitButton = (Button) findViewById(R.id.submit_button);
+        radioButton1 = (RadioButton) findViewById(R.id.radio_button_1);
+        radioButton2 = (RadioButton) findViewById(R.id.radio_button_2);
+        radioButton3 = (RadioButton) findViewById(R.id.radio_button_3);
+
         checkBoxListener();
+        radioButtonListener();
     }
 
     public void checkBoxListener() {
@@ -53,6 +69,25 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "Checkbox 3 is checked.", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(MainActivity.this, "Checkbox 3 is unchecked.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    public void radioButtonListener() {
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (((RadioButton) radioButton1).isChecked()) {
+                    Toast.makeText(MainActivity.this, "Radio Button 1 is checked.", Toast.LENGTH_SHORT).show();
+                } else if (((RadioButton) radioButton2).isChecked()) {
+                    Toast.makeText(MainActivity.this, "Radio Button 2 is checked.", Toast.LENGTH_SHORT).show();
+                } else if (((RadioButton) radioButton3).isChecked()) {
+                    Toast.makeText(MainActivity.this, "Radio Button 3 is checked.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "No Radio Button is checked.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
